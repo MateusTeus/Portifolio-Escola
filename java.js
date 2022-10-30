6aa110dbd7ff605620ba249a21d4971afae71df0
Mateus Henrique <mateusteccruz@gmail.com>
2022-10-30 20:28:24 -0300

    Versao com javascript e finalizacao do site

---
+++ java.js
@@ -0,0 +1,70 @@
+function mudar_claro(){
+    
+    document.getElementById("conteudo_id").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_container_imagem").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
+    document.getElementById("id_container_segundo").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
+    document.getElementById("sect-conhecimentos").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
+    document.getElementById("container_linguagens_id").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
+    document.getElementById("id_container_rodape").style.background = 
+
+    document.getElementById("id_item1").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+    document.getElementById("id_item2").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+    document.getElementById("id_item3").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+    document.getElementById("id_item4").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+    document.getElementById("id_item5").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+    document.getElementById("id_item6").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+
+
+    document.getElementById("conteudo_id").style.color = "black";
+    document.getElementById("sect-sobre-mim").style.color = "black";
+    document.getElementById("sect-conhecimentos").style.color = "black";
+    document.getElementById("titulo_id_rodape").style.color = "black";
+
+    document.getElementById("titulo_id_rodape").style.borderColor = "#f57c7ce0";
+    document.getElementById("sect-apresentacao").style.borderColor = "#f57c7ce0";
+    document.getElementById("id_container_segundo").style.borderColor = "#f57c7ce0";
+    document.getElementById("sect-conhecimentos").style.borderColor = "#f57c7ce0";
+    document.getElementById("id_container_rodape").style.borderColor = "#f57c7ce0";
+}
+
+function mudar_escuro(){
+
+    document.getElementById("titulo_id_rodape").style.borderColor = "black";
+    document.getElementById("sect-apresentacao").style.borderColor = "black";
+    document.getElementById("id_container_segundo").style.borderColor = "black";
+    document.getElementById("sect-conhecimentos").style.borderColor = "black";
+    document.getElementById("id_container_rodape").style.borderColor = "black";
+
+
+    document.getElementById("id_item1").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+    document.getElementById("id_item2").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+    document.getElementById("id_item3").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+    document.getElementById("id_item4").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+    document.getElementById("id_item5").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+    document.getElementById("id_item6").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+
+    document.getElementById("container_linguagens_id").style.background = "linear-gradient(to right, #373d42, #262a2d)";
+    document.getElementById("conteudo_id").style.background = "linear-gradient(to right,#373d42 , #262a2d)";
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#373d42, #262a2d)";
+    document.getElementById("id_container_imagem").style.background = "linear-gradient(to right,#373d42, #262a2d)";
+    document.getElementById("id_container_segundo").style.background = "linear-gradient(to right,#373d42, #262a2d)";
+    document.getElementById("sect-conhecimentos").style.background = "linear-gradient(to right,#373d42, #262a2d)";
+    document.getElementById("id_container_rodape").style.background = "linear-gradient(to right, #373d42, #262a2d)";
+   
+
+
+    document.getElementById("conteudo_id").style.color = "white";
+    document.getElementById("sect-sobre-mim").style.color = "white";
+    document.getElementById("sect-conhecimentos").style.color = "white";
+    document.getElementById("titulo_id_rodape").style.color = "white";
+
+
+
+}
+
+window.alert("Ao lado direito do cabecalho tem a opção TEMA. Selecione: claro(padrão) ou escuro.")
+
+
+
+
